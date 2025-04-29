6dc2c11b8a24c52453b0f0d820418df1f659c877
MalikAhsan123 <130451218+MalikAhsan123@users.noreply.github.com>
2025-04-29 12:35:11 +0500

    Create project

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-red-500'>Mern Blog Project</div>
+     </>
+  )
+}
+
+export default App
